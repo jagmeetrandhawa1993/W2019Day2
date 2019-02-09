b789c663c988827009fc35a190a405f3881b14d9
MacStudent <macstudent@126-iMac02.local>
2019-02-08 19:36:53 -0500

    Array slice

--- W2019Day2/main.swift
+++ W2019Day2/main.swift
@@ -63,3 +63,66 @@ for i in z
 }
 
 
+// Array
+
+//Array declaration
+var u = [10,20,30,40,50]
+
+print(u.count)
+
+//Printing array values
+for i in u
+{
+    print(i)
+}
+
+print(u[4])
+
+u[4]=1000
+print(u[4])
+
+var v = [Int]()
+
+//swift is mutable so values can be added using append
+v.append(100) //assining value to v
+v.append(200)
+v.append(300)
+v[0] = 10
+
+//use of +=
+v += [111,222,333]
+print(v[0])
+
+print(v[0],v[1],v[2])
+
+print("values of V array")
+let w = u+v
+for i in w
+{
+    print(i)
+}
+print("values of w[1..3] array" )
+
+for i in w
+{
+    print(i)
+}
+
+//Array slice 
+var q = w[1...3]
+print(q)
+q[1] = 2020
+for i in q
+{
+ print(i)
+}
+
+print("values of W[1] array")
+print(w[1])
+
+// Declaration using Array<Type>()
+var r=Array<Int>()
+r.append(100)
+print(r)
+
+
